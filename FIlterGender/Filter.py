519b51d336b1b544579c862e87ca49b5cfc6d479
Franco Primerano <franco28xl@gmail.com>
2024-09-28 15:38:15 -0300

    feat: added baseline FilterGender

--- FIlterGender/Filter.py
+++ FIlterGender/Filter.py
@@ -9,6 +9,23 @@ class Filter(Node):
         data = []
         return data
     
+    def send_eof(self):
+        logging.info("action: eof")
+
+    def is_gender(self, genders, wanted_gender):
+        for gender in genders.split(','):
+            if gender == wanted_gender:
+                return True
+        return False
+    
+    def send_game(self, data, gender):
+        logging.info(f"action: result | name: {data['name']} | routing key: {gender}")
+
     def process_data(self, data):
         if self.is_eof(data):
+            self.send_eof()
             return
+        if self.is_gender(data["gender"], "indie"):
+            self.send_game(data, "indie")
+        if self.is_gender(data["gender"], "action"):
+            self.send_game(data, "action")
