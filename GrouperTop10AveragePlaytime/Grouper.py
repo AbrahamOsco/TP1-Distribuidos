c74ae24693bc4965bb44f9330ba4d15351daf074
Franco Primerano <franco28xl@gmail.com>
2024-09-28 16:20:51 -0300

    refactor: make top size a variable

--- GrouperTop10AveragePlaytime/Grouper.py
+++ GrouperTop10AveragePlaytime/Grouper.py
@@ -5,6 +5,7 @@ class Grouper(Node):
     def __init__(self):
         super()
         self.reset_list()
+        self.top_size = 10
 
     def reset_list(self):
         self.list = []
@@ -15,7 +16,7 @@ class Grouper(Node):
         return data
 
     def has_to_be_inserted(self, game):
-        return len(self.list) < 10 or game["average_playtime"] > self.min_time
+        return len(self.list) < self.top_size or game["average_playtime"] > self.min_time
     
     def send_result(self):
         logging.info(f"action: result | list: {self.list}")
@@ -30,6 +31,6 @@ class Grouper(Node):
                 if data["average_playtime"] > self.list[i]["average_playtime"]:
                     self.list.insert(i, data)
                     break
-            if len(self.list) > 10:
+            if len(self.list) > self.top_size:
                 self.list.pop()
             self.min_time = self.list[-1]["average_playtime"]
--- GrouperTop5ReviewsPositiveIndie/Grouper.py
+++ GrouperTop5ReviewsPositiveIndie/Grouper.py
@@ -5,6 +5,7 @@ class Grouper(Node):
     def __init__(self):
         super()
         self.reset_list()
+        self.top_size = 5
 
     def reset_list(self):
         self.list = []
@@ -15,7 +16,7 @@ class Grouper(Node):
         return data
 
     def has_to_be_inserted(self, game):
-        return len(self.list) < 5 or game["reviews"] > self.min_time
+        return len(self.list) < self.top_size or game["reviews"] > self.min_time
     
     def send_result(self):
         logging.info(f"action: result | list: {self.list}")
@@ -30,6 +31,6 @@ class Grouper(Node):
                 if data["reviews"] > self.list[i]["reviews"]:
                     self.list.insert(i, data)
                     break
-            if len(self.list) > 5:
+            if len(self.list) > self.top_size:
                 self.list.pop()
             self.min_time = self.list[-1]["reviews"]
