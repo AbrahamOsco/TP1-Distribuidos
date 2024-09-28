79d9c358a073afbfd1164b346cc453a2ecf38bc5
Franco Primerano <franco28xl@gmail.com>
2024-09-28 15:58:57 -0300

    feat: added FilterScore5KPositives baseline

---
+++ FilterScore5KPositives/Filter.py
@@ -0,0 +1,26 @@
+import logging
+from common.node.node import Node
+
+class Filter(Node):
+    def __init__(self):
+        super()
+
+    def receive_data(self):
+        data = []
+        return data
+    
+    def send_eof(self):
+        logging.info("action: eof")
+
+    def has_enough_score(self, score):
+        return score >= 5000
+    
+    def send_game(self, data):
+        logging.info(f"action: result | name: {data['name']}")
+
+    def process_data(self, data):
+        if self.is_eof(data):
+            self.send_eof()
+            return
+        if self.has_enough_score(data["score"]):
+            self.send_game(data)
