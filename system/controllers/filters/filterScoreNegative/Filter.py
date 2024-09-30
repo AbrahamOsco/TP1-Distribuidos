9b639f992121244fb7e0667b61e50c7ff597f52c
Kevin Gadacz <kgadacz@fi.uba.ar>
2024-09-30 18:11:25 -0300

    se agrega logica para filterScoreNegative

--- system/controllers/filters/filterScoreNegative/Filter.py
+++ system/controllers/filters/filterScoreNegative/Filter.py
@@ -3,3 +3,13 @@ from commonsSystem.node.node import Node
 class Filter(Node):
     def __init__(self):
         super()
+    
+    def trim_data(self, data):
+        return data.retain(["client", "id", "review_text"])
+
+    def send_review(self, data):
+        self.broker.public_message(exchange_name=self.sink, message=self.trim_data(data).to_string())
+
+    def process_data(self, data):
+        if data["review_score"] == 0:
+            self.send_review(data)
