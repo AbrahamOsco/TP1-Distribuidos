7fa7e9e0342d4f5483223434b2eb37c4fb767787
Kevin Gadacz <kgadacz@fi.uba.ar>
2024-09-30 09:59:24 -0300

    muevo selectidname a la carpeta select y actualizo generar dockerfile

--- generar_docker_compose.py
+++ generar_docker_compose.py
@@ -101,7 +101,7 @@ services:
     # client_services += generar_servicios("SelectQ2345", select_q2345) TODO: No se encuentra implementado
     # client_services += generar_servicios("filterGender", filter_gender) TODO: No se encuentra implementado
     client_services += generar_servicios("filters","filterDecade", filter_decade)
-    # client_services += generar_servicios("SelectIDName", select_id_name) TODO: No se encuentra implementado
+    client_services += generar_servicios("select","SelectIDName", select_id_name)
     # client_services += generar_servicios("SelectQ345", select_q345) TODO: No se encuentra implementado
     # client_services += generar_servicios("FilterScorePositive", filter_score_positive) TODO: No se encuentra implementado
     client_services += generar_servicios("filters","filterReviewEnglish", filter_review_english)
