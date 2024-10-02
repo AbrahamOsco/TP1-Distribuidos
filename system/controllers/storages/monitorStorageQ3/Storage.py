9ce59c24fb51c6d9ab7b8034985fe88294bce393
Franco Primerano <franco28xl@gmail.com>
2024-10-02 16:39:01 -0300

    feat: finished storageQ3

--- system/controllers/storages/monitorStorageQ3/Storage.py
+++ system/controllers/storages/monitorStorageQ3/Storage.py
@@ -1,5 +1,6 @@
 import logging
 import os
+import heapq
 from system.commonsSystem.node.node import Node, UnfinishedBusinessException
 
 STATUS_STARTED = 0
@@ -13,19 +14,32 @@ class Storage(Node):
 
     def reset_list(self):
         self.list = {}
+        self.top_5_heap = []
         self.status = STATUS_STARTED
 
     def inform_eof_to_nodes(self, client):
         if self.status == STATUS_REVIEWING:
+            self.send_result()
             self.send_eof(client)
             self.reset_list()
             self.status = STATUS_STARTED
         else:
             self.status = STATUS_REVIEWING
 
-    def send_review(self, data):
-        logging.info(f"action: result | list: {self.list}")
+    def send_result(self):
+        data = self.get_top_5()
+        logging.info(f"action: result | list: {data}")
         self.broker.public_message(exchange_name=self.sink, message=data, routing_key="default")
+
+    def get_top_5(self):
+        return sorted(self.top_5_heap, key=lambda x: -x[0])
+    
+    def _update_heap(self, name):
+        if len(self.top_5_heap) == 5:
+            if self.data[name] > self.top_5_heap[0][0]:
+                heapq.heappushpop(self.top_5_heap, (self.data[name], name))
+        else:
+            heapq.heappush(self.top_5_heap, (self.data[name], name))
 
     def process_data(self, data):
         if data.is_review():
@@ -33,6 +47,7 @@ class Storage(Node):
                 raise UnfinishedBusinessException()
             if data.app_id in self.list:
                 self.list[data.app_id] += 1
+                self._update_heap(data.app_id)
         if data.is_game():
             if self.status == STATUS_REVIEWING:
                 raise UnfinishedBusinessException()
