c13ede48fda01d320f105a01c2c8be951c0440c0
Kevin Gadacz <kgadacz@fi.uba.ar>
2024-10-04 11:48:57 -0300

    Revert "revert merge"
    This reverts commit 53eedf40a9ab4bde13e6e3f1e0ee18c220cfa577.

--- run.sh
+++ run.sh
@@ -3,12 +3,10 @@ chmod +x run.sh stop.sh
 docker build -f ./system/rabbitmq/Dockerfile -t "rabbit:latest" .
 docker build -f ./system/controllers/gateway/Dockerfile -t "gateway:latest" .
 docker build -f ./system/controllers/filters/filterBasic/Dockerfile -t "filterbasic:latest" .
-# docker build -f ./system/controllers/select/selectQ1/Dockerfile -t "selectq1:latest" .
-docker build -f ./system/controllers/select/selectQ2345/Dockerfile -t "selectq2345:latest" .
-docker build -f ./system/controllers/filters/filterGender/Dockerfile -t "filtergender:latest" .
+docker build -f ./system/controllers/select/selectQ1/Dockerfile -t "selectq1:latest" .
 docker build -f ./client/Dockerfile -t "client:latest" .
-# docker build -f ./system/controllers/groupers/platformCounter/Dockerfile -t "platformcounter:latest" .
-# docker build -f ./system/controllers/reducers/platformReducer/Dockerfile -t "platformreducer:latest" .
+docker build -f ./system/controllers/groupers/platformCounter/Dockerfile -t "platformcounter:latest" .
+docker build -f ./system/controllers/reducers/platformReducer/Dockerfile -t "platformreducer:latest" .
 
 #docker build -f ./system/controllers/select/selectIDName/Dockerfile -t "selectidname:latest" .
 #docker build -f ./system/controllers/filters/filterDecade/Dockerfile -t "filterdecade:latest" .
