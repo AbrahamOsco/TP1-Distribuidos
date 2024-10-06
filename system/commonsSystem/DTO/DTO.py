442c615001815b5ead8637a9482f0fb7c923d5de
Franco Primerano <franco28xl@gmail.com>
2024-10-06 14:00:07 -0300

    feat: optimized strings DTO

--- system/commonsSystem/DTO/DTO.py
+++ system/commonsSystem/DTO/DTO.py
@@ -1,4 +1,11 @@
 from system.commonsSystem.DTO.enums.OperationType import OperationType
+
+type_to_length = {
+    OperationType.OPERATION_TYPE_SHORT_STR.value: 1,
+    OperationType.OPERATION_TYPE_MID_STR.value: 2,
+    OperationType.OPERATION_TYPE_LONG_STR.value: 3,
+}
+
 class DTO:
     def __init__(self):
         pass
@@ -6,15 +13,25 @@ class DTO:
     def serialize_str(a_string:str):
         serialized_string = bytearray()
         string_in_bytes = a_string.encode('utf-8')
-        serialized_string.extend(OperationType.OPERATION_TYPE_STR.value.to_bytes(1, byteorder='big'))
-        serialized_string.extend(len(string_in_bytes).to_bytes(3, byteorder='big'))
+        string_length = len(string_in_bytes)
+        if string_length < 256:
+            serialized_string.extend(OperationType.OPERATION_TYPE_SHORT_STR.value.to_bytes(1, byteorder='big'))
+            serialized_string.extend(string_length.to_bytes(1, byteorder='big'))
+        elif string_length < 65536:
+            serialized_string.extend(OperationType.OPERATION_TYPE_MID_STR.value.to_bytes(1, byteorder='big'))
+            serialized_string.extend(string_length.to_bytes(2, byteorder='big'))
+        else:
+            serialized_string.extend(OperationType.OPERATION_TYPE_LONG_STR.value.to_bytes(1, byteorder='big'))
+            serialized_string.extend(string_length.to_bytes(3, byteorder='big'))
         serialized_string.extend(string_in_bytes)
         return serialized_string
 
     def deserialize_str(data, offset):
+        operation_type = int.from_bytes(data[offset:offset+1], byteorder='big')
         offset += 1
-        string_length = int.from_bytes(data[offset:offset+3], byteorder='big')
-        offset += 3
+        length_length = type_to_length[operation_type]
+        string_length = int.from_bytes(data[offset:offset+length_length], byteorder='big')
+        offset += length_length
         string = data[offset:offset + string_length].decode('utf-8')
         offset += string_length
         return string, offset
