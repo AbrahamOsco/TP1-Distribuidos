94ebf1b2515565bcd40c7098d91022055e95a1e2
Franco Primerano <franco28xl@gmail.com>
2024-10-09 15:59:29 -0300

    feat: modified EOF DTO

--- system/commonsSystem/DTO/EOFDTO.py
+++ system/commonsSystem/DTO/EOFDTO.py
@@ -1,13 +1,16 @@
+STATE_DEFAULT = 1
+STATE_COMMIT = 2
+STATE_OK = 3
+STATE_FINISH = 4
+
 class EOFDTO:
-    def __init__(self, type, client:int, confirmation=True, amount_received = 0, amount_sent = 0):
+    def __init__(self, type, client:int, state:int, attribute="", amount_received = 0, amount_sent = 0):
         self.operation_type = type
+        self.state = state
         self.client = client
-        self.confirmation = confirmation
+        self.attribute = attribute
         self.amount_sent = amount_sent
         self.amount_received = amount_received
-
-    def is_confirmation(self):
-        return self.confirmation
     
     def get_client(self):
         return self.client
@@ -25,7 +28,7 @@ class EOFDTO:
         eof_bytes = bytearray()
         eof_bytes.extend(self.operation_type.value.to_bytes(1, byteorder='big'))
         eof_bytes.extend(self.client.to_bytes(1, byteorder='big'))
-        eof_bytes.extend(self.confirmation.to_bytes(1, byteorder='big'))
+        eof_bytes.extend(self.state.to_bytes(1, byteorder='big'))
         eof_bytes.extend(self.amount_received.to_bytes(4, byteorder='big'))
         eof_bytes.extend(self.amount_sent.to_bytes(4, byteorder='big'))
         return bytes(eof_bytes)
@@ -36,10 +39,10 @@ class EOFDTO:
         offset += 1
         client = int.from_bytes(data[offset:offset+1], byteorder='big')
         offset += 1
-        confirmation = int.from_bytes(data[offset:offset+1], byteorder='big')
+        state = int.from_bytes(data[offset:offset+1], byteorder='big')
         offset += 1
         amount_received = int.from_bytes(data[offset:offset+4], byteorder='big')
         offset += 4
         amount_sent = int.from_bytes(data[offset:offset+4], byteorder='big')
         offset += 4
-        return EOFDTO(operation_type, client, confirmation, amount_received, amount_sent), offset
+        return EOFDTO(operation_type, client, state, amount_received, amount_sent), offset
