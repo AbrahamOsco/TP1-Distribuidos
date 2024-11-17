b2a0774e08025868629c6d957f8e9096102ee750
Abraham Osco <aosco@fi.uba.ar>
2024-11-17 01:29:56 -0300

    fix get_host_name in cases of  system's nodes

--- system/commonsSystem/utils/connectionLeader.py
+++ system/commonsSystem/utils/connectionLeader.py
@@ -6,10 +6,10 @@ OFFSET_MEDIC_HOSTNAME = 500
 def get_host_name(a_id: str):
     a_id = int(a_id)
     node_name = os.getenv("NODE_NAME")
-    number_linked_node = 100
     if a_id >= OFFSET_MEDIC_HOSTNAME:
         number_linked_node = OFFSET_MEDIC_HOSTNAME
-    return f"{node_name}_{a_id-number_linked_node}"
+        return f"{node_name}_{a_id-number_linked_node}"
+    return f"{node_name}"
 
 def get_service_name(a_id: int):
     a_id = int(a_id)
